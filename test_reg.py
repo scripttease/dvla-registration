aa80e802888eebe9579e9d74a3d342027f4334e0
Al Dee <amyalicedee@gmail.com>
2023-08-11 10:34:58 +0100

    Add get-all test

--- test_reg.py
+++ test_reg.py
@@ -1,6 +1,13 @@
 import pytest
 from reg import *
 
+# NOTE:
+# depending on desired behavior I might implement:
+# TODO: preventing duplicate keys in register method
+# TODO: Edge case tests depending on desired behaviour
+# TODO: Validation on registration length.
+# TODO: Validation on registration format.
+
 
 @pytest.fixture
 def database():
@@ -35,3 +42,11 @@ def test_total_registration_numbers(database):
 def test_get_vehicle(database):
     reg = "AB01 CDE"
     assert database.get_vehicle(reg) == Vehicle("AB01 CDE", 2001, 12345)
+
+
+def test_get_vehicles(database):
+    assert database.get_vehicles() == [
+        Vehicle("AB01 CDE", 2001, 12345),
+        Vehicle("FG02 HJK", 2001, 12345),
+        Vehicle("L33TH 4X0R", 2022, None),
+    ]
